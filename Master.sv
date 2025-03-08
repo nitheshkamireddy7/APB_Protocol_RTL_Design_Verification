5e365e759145675d734b77e0ea14169fcb1a7eb7
nitheshkamireddy7 <nitheshkamireddy2024@gmail.com>
2025-03-07 23:12:59 -0800

    Update Master.sv

--- Master.sv
+++ Master.sv
@@ -1,5 +1,20 @@
 module APB_master (
+  input logic clk,
+  input logic resetn, // Asynchronous active low reset
+  input logic pwrite,
+  input logic [4:0] addr,
+  input logic psel,
+  input logic penable,
+  input logic [3:0] pstrobe,
+  input logic [2:0] Prot,
+  input logic [31:0] pwdata,
 
+  // Outputs from APB_slave
+  output logic pready,
+  output logic pslverr,
+  output logic [31:0] prdata
   
 );
+
+  APB_slave bus(
 endmodule
